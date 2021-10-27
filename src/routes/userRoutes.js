c32a71e47a0f54700ac57facf2c77f8821451ad1
orionalfa <orionalfacentauri@gmail.com>
2021-10-27 16:54:01 +0200

    user registration and get by email endpoints

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,15 @@
 const Router = require("express").Router;
 const userRouter = Router();
 const { userController } = require("../controllers");
-const { authMiddleware } = require("../middlewares");
+// const { authMiddleware } = require("../middlewares");
 
 //end points routes:
 
+//POST
+userRouter.post("/register", userController.register);
+
 //GET
-userRouter.get("/get-user/:id", authMiddleware, userController.getById);
+userRouter.get("/get-user/:id", userController.getById);
+userRouter.get("/get-email/:email", userController.getByEmail);
 
 module.exports = userRouter;
